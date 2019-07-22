ca8a6b445053433af1026dc01fc924bc2ceaa6fd
huoxiaoye <17711034@bjtu.edu.cn>
2019-07-22 15:49:26 +0800

    修复bug

--- node/dao/dbase.js
+++ node/dao/dbase.js
@@ -1,49 +1,52 @@
-const  MongoClient = require("mongodb").MongoClient
+const MongoClient = require("mongodb").MongoClient
 
 
 class xiaoye_db {
-	constructor(urlName,databaseName){
+	constructor(urlName, databaseName) {
 		this.urlName = urlName;
 		this.databaseName = databaseName;
 	}
-	_connect(callback){
-		MongoClient.connect(this.urlName,{useNewUrlParser : true},(err,db)=>{
-			callback(err,db);
+	_connect(callback) {
+		MongoClient.connect(this.urlName, { useNewUrlParser: true }, (err, db) => {
+			callback(err, db);
 			db.close() // 关闭数据库
 		})
 	}
-	insert(collectionName,json,callback){
-		this._connect((err,db)=>{
-			if(err) return console.log("连接数据库失败");
+	insert(collectionName, json, callback) {
+		this._connect((err, db) => {
+			if (err) return console.log("连接数据库失败");
 			let dbase = db.db(this.databaseName);
-			if(!Array.isArray(json)){ // 添加一条数据
-				dbase.collection(collectionName).insertOne(json,(err,result)=>{
-					return callback&&callback(err,result)
+			console.log(!Array.isArray(json))
+			if (!Array.isArray(json)) { // 添加一条数据
+				dbase.collection(collectionName).insertOne(json, (err, result) => {
+					return callback && callback(err, result)
+				})
+			} else {
+				// 添加多条数据
+				dbase.collection(collectionName).insertMany(json, (err, result) => {
+					return callback && callback(err, result)
 				})
 			}
-			// 添加多条数据
-			dbase.collection(collectionName).insertMany(json,(err,result)=>{
-				return callback&&callback(err,result)
-			})
+
 		})
 	}
-	find(collectionName,json,args,callback){
-		this._connect((err,db)=>{
-			if(err) return console.log("数据库连接失败")
-			if(arguments.length == 3 && typeof args == "function"){
+	find(collectionName, json, args, callback) {
+		this._connect((err, db) => {
+			if (err) return console.log("数据库连接失败")
+			if (arguments.length == 3 && typeof args == "function") {
 				callback = args;
 				var skipAmount = 0;
 				var limitNumber = 0;
 				var sortObj = {};
-			}else{
+			} else {
 				var skipAmount = args.pageSize * args.page;
 				var limitNumber = args.pageSize;
 				var sortObj = args.sort;
 			}
 			let dbase = db.db(this.databaseName)
-			dbase.collection(collectionName).find(json).limit(limitNumber).skip(skipAmount).sort(sortObj).toArray((err,result)=>{
+			dbase.collection(collectionName).find(json).limit(limitNumber).skip(skipAmount).sort(sortObj).toArray((err, result) => {
 				// console.log(result)
-				callback&&callback(err,result)
+				callback && callback(err, result)
 			})
 		})
 	}
--- node/router/router.js
+++ node/router/router.js
@@ -14,21 +14,21 @@ exports.doLogin = (req, res) => {
     req.on("end", () => {
         let data = JSON.parse(namePassword.toString())
         xiaoye.find("userInfo", { "username": data.username }, (err, result) => {
-            if(result.length == 0){
-                res.send({"result":"-2"})
+            if (result.length == 0) {
+                res.send({ "result": "-2" })
                 return res.end()
             }
             if (result[0].password == data.password) { // 成功
-                res.send({"result":"1"})
+                res.send({ "result": "1" })
                 return res.end()
             }
-            res.send({"result":"-1"})
+            res.send({ "result": "-1" })
             res.end()
         })
     })
 }
 
-exports.doRegist = (req,res)=>{
+exports.doRegist = (req, res) => {
     let namePassword = ""
     req.on("data", (chunk) => {
         namePassword += chunk;
@@ -37,14 +37,19 @@ exports.doRegist = (req,res)=>{
         let data = JSON.parse(namePassword.toString())
         // 查找数据库中有没有这个用户名
         xiaoye.find("userInfo", { "username": data.username }, (err, result) => {
-            if(result.length == 0){ // 没有该用户 可以注册
-                xiaoye.insert("userInfo",data,(err,result)=>{
-                    res.send({"result":"1"})
+            if (result.length == 0) { // 没有该用户 可以注册
+
+                // res.send({ "result": "1" })
+                // return res.end()
+
+                xiaoye.insert("userInfo", data, (err, result) => {
+                    res.json({ "result": "1" })
                     return res.end()
                 })
-            }else if (result.length == 1) { // 该用户名已存在
-                res.send({"result":"-1"})
-                return res.end()
+            } else if (result.length == 1) { // 该用户名已存在
+                res.send({ "result": "-1" })
+                return
+
             }
         })
     })
