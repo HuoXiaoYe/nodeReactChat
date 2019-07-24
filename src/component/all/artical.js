c28ce1e2636c458f907eb6af4cd13cd99e2e0794
huoxiaoye <17711034@bjtu.edu.cn>
2019-07-24 13:26:09 +0800

    查看动态详情

--- src/component/all/artical.js
+++ src/component/all/artical.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Comment, Tooltip, Avatar } from 'antd';
+import { Comment, Avatar, Modal, Button } from 'antd';
 import moment from 'moment';
 
 
@@ -8,13 +8,14 @@ class Artical extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data : this.props.data
+            data: this.props.data,
+            visible: false,
         }
     }
     render() {
         let data = this.state.data;
         return (
-            <div style={{ padding: '0 100px', lineHeight: '20px' }}>
+            <div style={{ padding: '0 100px', lineHeight: '20px' }} onClick={this.showModal}>
                 <Comment
                     author={<a>{data.name}</a>}
                     avatar={
@@ -23,32 +24,67 @@ class Artical extends Component {
                     content={
                         <p style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', width: "100%" }}>
                             {data.content}
-                    </p>
+                        </p>
                     }
                     datetime={
-                            <span>
-                                {moment()
-                                    .subtract(this.getTime(data.datetime) , 'hours')
-                                    .fromNow()}
-                            </span>
+                        <span>
+                            {moment()
+                                .subtract(this.getTime(data.datetime), 'hours')
+                                .fromNow()}
+                        </span>
                     }
                 />
+
+
+
+                <Modal
+                    title={data.name + ":"}
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                   
+                >
+                    <p>{data.content}</p>
+                </Modal>
+
+
+
+
             </div>
         );
     }
-    componentWillReceiveProps(nextProps){
+    componentWillReceiveProps(nextProps) {
         this.setState({
-            data : nextProps.data
+            data: nextProps.data
         })
     }
-    getTime(time){
+    getTime(time) {
         let oldDate = new Date(time).getTime();
         let nowDate = new Date().getTime();
         let dif = nowDate - oldDate; // 毫秒值
-        let hours =  dif / 1000 / 60/ 60 
-        return(hours)
+        let hours = dif / 1000 / 60 / 60
+        return (hours)
 
     }
+    showModal = () => {
+        this.setState({
+          visible: true,
+        });
+      };
+    
+      handleOk = e => {
+        // console.log(e);
+        this.setState({
+          visible: false,
+        });
+      };
+    
+      handleCancel = e => {
+        // console.log(e);
+        this.setState({
+          visible: false,
+        });
+      };
 }
 
 export default Artical;
