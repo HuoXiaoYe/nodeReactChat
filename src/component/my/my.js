50f09874e39062fcff58c0a9977dae7e02c099b6
huoxiaoye <17711034@bjtu.edu.cn>
2019-07-30 21:34:39 +0800

    个人说说页面布局完成

--- src/component/my/my.js
+++ src/component/my/my.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 
+import { Input, Form, List, Typography } from 'antd';
+
+const { TextArea } = Input;
+
+
+const data = []
+
 class My extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            myList: []
+        }
     }
-    render() { 
-        return ( 
-            <div>个人说说页面</div>
-         );
+    render() {
+        return (
+            <div className="my-container" style={{ padding: 30, width: "60%" }}>
+                {/* 头部发表说说页面 */}
+                <div className="header">
+
+                    <Form.Item label="发表说说" style={{ fontSize: 24 }}>
+                        <Input.TextArea style={{ marginTop: 20 }} id="intr" autosize={{ minRows: 4, maxRows: 6 }} />
+                    </Form.Item>
+
+                </div>
+                {/* 呈现个人说说页面 */}
+                <div className="list">
+                    <List
+                        header={<div>我的说说</div>}
+                        bordered
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                            </List.Item>
+                        )}
+                    />
+
+
+                </div>
+
+            </div>
+        );
     }
 }
- 
+
 export default My;
